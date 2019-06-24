0f9a492310f00c174c78cab1828cdbd773f01647
Narinari0601 <kouta19960601.kizuna-reo.t-mk@ezweb.ne.jp>
2019-06-25 00:26:34 +0900

    playerのカメラに制限を

--- Exit/Assets/Scripts/CameraController.cs
+++ Exit/Assets/Scripts/CameraController.cs
@@ -10,10 +10,20 @@ public class CameraController : MonoBehaviour
 
     public float sensitivity = 0;  //カメラ感度
 
+    //float angle;
+
+    Vector3 roteuler;  //オイラー角
+
+    public float minRotateX = 0;
+    public float maxRotateX = 0;
+
 
     void Start()
     {
         offset = transform.position - target.transform.position;
+
+        //angle = transform.localEulerAngles.x;
+        roteuler = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0f);  //親オブジェクトのオイラー角取得
     }
 
     // Update is called once per frame
@@ -26,8 +36,14 @@ public class CameraController : MonoBehaviour
 
     private void CameraMouseRotation()
     {
-        Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * sensitivity, -Input.GetAxis("Mouse Y") * sensitivity, 0);
-        transform.RotateAround(transform.position, Vector3.up, angle.x);
-        transform.RotateAround(transform.position, transform.right, angle.y);
+        float rotateX = Input.GetAxis("Mouse X");
+        float rotateY = Input.GetAxis("Mouse Y");
+
+        //Vector3 angle = new Vector3(rotateX * sensitivity, -rotateY * sensitivity, 0);
+        //transform.RotateAround(transform.position, transform.right, angle.y);
+        //transform.RotateAround(transform.position, Vector3.up, angle.x);
+
+        roteuler = new Vector3(Mathf.Clamp(roteuler.x - rotateY, minRotateX, maxRotateX), roteuler.y + rotateX, 0f);  //Mathf.Clampで角度制限
+        transform.localEulerAngles = roteuler;
     }
 }
