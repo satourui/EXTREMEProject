8bc67eee10d99b75a352ee39f7a41ea1659f3db5
佐藤 <satourui7802@gmail.com>
2019-06-24 11:33:20 +0900

    Project作成。ガントチャート本作成。

---
+++ Exit/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+
+    [SerializeField]
+    private float speed = 3.0f;
+    private float gravity = 9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMove();
+    }
+
+    void CalculateMove()
+    {
+        float horizontallnput = Input.GetAxis("Horizontal");
+        float verticallnput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontallnput, 0, verticallnput);
+        Vector3 velocity = direction * speed;
+        velocity.y -= gravity;
+        velocity = transform.transform.TransformDirection(velocity);
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
