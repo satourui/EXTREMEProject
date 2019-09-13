907c9a09678da7c4b85b66dce1da1421a47bbca8
佐藤幹 <->
2019-09-13 13:51:49 +0900

    Door関連

---
+++ Exit/Assets/Scripts/Door/DoorChaild.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorChaild : MonoBehaviour
+{
+    public bool b;
+    // Start is called before the first frame update
+    void Start()
+    {
+        b = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider o)
+    {
+        if(o.tag=="Enemy")
+        {
+            b = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Enemy")
+        {
+            b = false;
+        }
+    }
+}
