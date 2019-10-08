8e48ef746f23a6750601b6b3c6deba19556c07b2
佐藤 <satourui7802@gmail.com>
2019-10-08 10:13:07 +0900

    10月の予定とタイトル画面作成

---
+++ Exit/Assets/Scripts/Fade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    //フェード処理に必要な値達
+    float alpha;//透明値を操作するための値
+    float red, green, blue;//RGBを操作するための値
+    public float speed = 0.1f; //フェードの速度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //暗転パネルの各色を取得
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alpha);
+        alpha += speed;
+    }
+}
---
+++ Exit/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    float count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count < 1.0f)
+        {
+            count = count + 0.1f;
+        }
+        else if(count > 1.0f)
+        {
+            Destroy(this);
+        }
+    }
+
+    public void OnRetry()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
