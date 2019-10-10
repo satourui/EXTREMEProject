4e06ca774d581667c33479c0ec8f053e4fbb3a8a
佐藤 <satourui7802@gmail.com>
2019-10-11 00:55:32 +0900

    タイトル背景追加

---
+++ Exit/Assets/Scripts/Scene/TitleFlashLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleFlashLight : MonoBehaviour
+{
+    Light light;
+    float lightIntensity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        lightIntensity = light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
