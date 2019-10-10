f1debdc17142086b86bbffe463680b432d76332b
Narinari0601 <kouta19960601.kizuna-reo.t-mk@ezweb.ne.jp>
2019-10-11 01:50:32 +0900

    タイトルのセレクト、遷移処理など

--- Exit/Assets/Scripts/Scene/TitleManager.cs
+++ Exit/Assets/Scripts/Scene/TitleManager.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
     //「▷」←のUI画像(今はチェック画像)
     [SerializeField]
-    public GameObject SelectUI;
+    public GameObject selectUI;
+    RectTransform selectUIRect;
+    
+
+    public GameObject[] selectButtons = new GameObject[0];  //選択ボタンを入れる配列
+
+    Vector3[] selectButtonsRectPositions;  //選択ボタンの位置配列
+
+    int selectButtonsValue;  //選択ボタンの数
+
+    int currentSelectButtonNum;  //現在の選択ボタンの番号
+
+    private float beforeTrigger;
+
+
+    private bool isGameStart;  //ゲームが始まるならtrue
+    private bool isTutorial;  //チュートリアルならtrue
+    private bool isGameEnd;  //ゲームをやめるならtrue
 
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
+
+        //選択ボタン関連の初期化
+        selectUIRect = selectUI.GetComponent<RectTransform>();
+
+        selectButtonsValue = selectButtons.Length;
+
+        selectButtonsRectPositions = new Vector3[selectButtonsValue];
+
+        for (int i = 0; i < selectButtonsValue; i++)
+        {
+            selectButtonsRectPositions[i] = selectButtons[i].GetComponent<RectTransform>().position;
+        }
+
+        currentSelectButtonNum = 0;
+
+        beforeTrigger = 0;
+
+        isGameStart = false;
+        isTutorial = false;
+        isGameEnd = false;
     }
 
 
@@ -21,31 +59,122 @@ public class TitleManager : MonoBehaviour
     void Update()
     {
         #region カーソルオンオフに関する処理(死ぬほどコードが汚く見辛いので訂正できるならよろしくお願いします)
+
+        ////マウスカーソルのオン キーボードを押したときにオフにする
+        //if (Input.GetAxis("Mouse X") > 0.1f || Input.GetAxis("Mouse X") < -0.1f
+        //    || Input.GetAxis("Mouse Y") > 0.1f || Input.GetAxis("Mouse Y") < -0.1f)
+        //{
+        //    Cursor.visible = true;
+        //    SelectUI.SetActive(false);
+        //}
+        //if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.S)||
+        //    Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    Cursor.visible = false;
+        //    SelectUI.SetActive(true);
+        //}
+
+
+        ////上キーとWを押したときにポジションを変えようとした跡地
+        //if (!Cursor.visible)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        //    {
+        //        SelectUI.transform.Translate(0,-1,0);
+        //    }
+        //}
+
+        #endregion
+
+
+        ChangeSelectUIPos();
+        SelectNextScene();
+
+        if (isGameStart)
+        {
+            GameStart();
+        }
+
+        if (isTutorial)
+        {
+
+        }
+
+        if (isGameEnd)
+        {
+            GameEnd();
+        }
+
         
-        //マウスカーソルのオン キーボードを押したときにオフにする
-        if (Input.GetAxis("Mouse X") > 0.1f || Input.GetAxis("Mouse X") < -0.1f
-            || Input.GetAxis("Mouse Y") > 0.1f || Input.GetAxis("Mouse Y") < -0.1f)
-        {
-            Cursor.visible = true;
-            SelectUI.SetActive(false);
-        }
-        if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.S)||
-            Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Cursor.visible = false;
-            SelectUI.SetActive(true);
-        }
+    }
 
+    void ChangeSelectUIPos()
+    {
+        float currentTrigger = Input.GetAxis("L_Stick_Verti");
 
-        //上キーとWを押したときにポジションを変えようとした跡地
-        if (!Cursor.visible)
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || (beforeTrigger == 0 && currentTrigger < 0))
         {
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            if (currentSelectButtonNum == selectButtonsValue - 1)
             {
-                SelectUI.transform.Translate(0,-1,0);
+                currentSelectButtonNum = 0;
+                beforeTrigger = currentTrigger;
+                return;
+            }
+
+            currentSelectButtonNum++;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || (beforeTrigger == 0 && currentTrigger > 0))
+        {
+            if (currentSelectButtonNum == 0)
+            {
+                currentSelectButtonNum = selectButtonsValue - 1;
+                beforeTrigger = currentTrigger;
+                return;
+            }
+
+            currentSelectButtonNum--;
+        }
+        
+        beforeTrigger = currentTrigger;
+
+        selectUIRect.position = selectButtonsRectPositions[currentSelectButtonNum] - new Vector3(30, 0, 0);
+    }
+
+    void SelectNextScene()
+    {
+        if (!isTutorial &&
+            (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.JoystickButton0)))
+        {
+            if (currentSelectButtonNum == 0)
+            {
+                isGameStart = true;
+            }
+
+            else if (currentSelectButtonNum == 1)
+            {
+                //isTutorial = true;
+            }
+
+            else
+            {
+                isGameEnd = true;
             }
         }
-        
-        #endregion
+    }
+
+    public void GameStart()
+    {
+        //ステージ１にシーン切り替え
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void GameEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
